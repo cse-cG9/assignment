3d27f9c3971d17119d37b30ad7fc63db7464fe54
cse-cG9 <nakkadivya951@gmail.com>
2025-02-24 18:20:51 +0530

    Add files via upload

---
+++ assignments.py
@@ -0,0 +1,22 @@
+'''
+Mary Teresa Bojaxhiu (born Anjezë Gonxhe Bojaxhiu, Albanian: [aˈɲɛzə ˈɡɔndʒɛ bɔjaˈdʒi.u];
+ 26 August 1910 – 5 September 1997), better known as Mother Teresa or Saint Mother Teresa
+ ,[a] was an Albanian-Indian Catholic nun, founder of cathelic
+'''
+# username="mary teresa"
+# date=1910
+# founder="catholic"
+# print(username)
+# print(date)
+# print(founder)
+
+'''
+Mohandas Karamchand Gandhi[c] (2 October 1869 – 30 January 1948)[2] was an Indian lawyer,
+ anti-colonial nationalist, and political ethicist who employed nonviolent resistance to lead the successful campaign for India's
+   independence from British rule.
+ He inspired movements for civil rights and freedom across the world. The h
+'''
+fullname=input("enter the fullname:")
+date=int(input("enter the date:"))
+rights=input("enter the rights:")
+print(f"fullname is {fullname} and date of birth is {date} and his rights are {rights}")
